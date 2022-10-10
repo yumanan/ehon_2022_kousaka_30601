78717ebbb281e3f5be6865c5442b8d28f802e7eb
DESKTOPC-yukkenan <sasimitabetaina@yahoo.co.jp>
2022-10-10 22:56:30 +0900

    画像系更新 slickとremodalとcsv系のプラグインを入れた

--- script.js
+++ script.js
@@ -65,68 +65,104 @@ $(function(){
     return false; 
     });
 
+    // ボタンをクリックすると、fallが消えて、flyが表示する処理
     $('a[href^="#to-top"]').click(function() {
-      $('a').removeClass('click-off');
-      
-      setTimeout(function(){
+      $('a.fly').removeClass('click-off');
+      $('a.fall').addClass('click-off');
+      return false; 
+    });
+    // topが上に行くとflyが消えて、fallが表示される
+    $(window).on('scroll', function(){
+      if ($(window).scrollTop() < 30) {
         $('a.fly').addClass('click-off');
-      },3000);
-      // 3秒ぐらい飛んでる状態
-      return false; 
+        $('a.fall').removeClass('click-off');
+      }
     });
 
-    // キャラ紹介左右ボタン
-    // 左
-    $('a[href^="#left1"]').click(function() {
-      $('div.cha-01').addClass('cha-none');
-      $('div.cha-03').removeClass('cha-none');
-      return false; 
+    // topへボタンの位置をto-topをfotterの下に入れて
+    // to-top-midを表示させる処理
+    var scroll_y = $("body").get(0).scrollHeight;
+    $(document).scroll(function(){
+      //ウィンドウの高さを取得
+      window_ht = $(window).height();
+
+      //スクロール量を取得
+      var scroll_p = $(document).scrollTop();
+      var scroll = scroll_p + window_ht;
+      if(scroll_y > scroll-350){
+        $('.to-top_mid').fadeOut(100);
+      }
+      else if(scroll_y < scroll){
+        $('.to-top_mid').fadeIn();
+      }
     });
-    $('a[href^="#left2"]').click(function() {
-      $('div.cha-02').addClass('cha-none');
-      $('div.cha-01').removeClass('cha-none');
-      return false; 
+
+    // slickのオプションの拡張
+    $(document).on('ready', function() {
+      $(".regular").slick({
+        arrows: true,
+        dots: true,
+        prevArrow: '<p class="slide-arrow prev-arrow left"></p>',
+        nextArrow: '<p class="slide-arrow next-arrow right"></p>',
+
+      });
     });
-    $('a[href^="#left3"]').click(function() {
-      $('div.cha-03').addClass('cha-none');
-      $('div.cha-02').removeClass('cha-none');
-      return false; 
+
+    // ここでmodel後に処理slick.jsの表示崩れを防止する為に再処理している
+    $(document).on('opening', '.remodal', function () {
+      console.log('Modal is opening');
+      $('.regular_tamesiyomi').slick('setPosition');
     });
-    //右
-    $('a[href^="#right1"]').click(function() {
-      $('div.cha-01').addClass('cha-none');
-      $('div.cha-02').removeClass('cha-none');
-      return false; 
-    });
-    $('a[href^="#right2"]').click(function() {
-      $('div.cha-02').addClass('cha-none');
-      $('div.cha-03').removeClass('cha-none');
-      return false; 
-    });
-    $('a[href^="#right3"]').click(function() {
-      $('div.cha-03').addClass('cha-none');
-      $('div.cha-01').removeClass('cha-none');
-      return false; 
+
+    // モーダル内のslick処理
+    $(".regular_tamesiyomi").slick({
+      dots: true,
     });
 
 
-    // 紹介3点ボタン
-    $('a[href^="#button1"]').click(function() {
-      $('div.cha-01').removeClass('cha-none');
-      $('div.cha-02').addClass('cha-none');
-      $('div.cha-03').addClass('cha-none');
-      return false; 
+    $('#send-csv-btn').click(function() {
+      var val = $('#coment-form [name=your-message]').val();
+      var val2 = $('#coment-form [name=your-name]').val();
     });
-    $('a[href^="#button2"]').click(function() {
-      $('div.cha-02').removeClass('cha-none');
-      $('div.cha-01').addClass('cha-none');
-      $('div.cha-03').addClass('cha-none');
-      return false; 
-    });
-    $('a[href^="#button3"]').click(function() {
-      $('div.cha-03').removeClass('cha-none');
-      $('div.cha-01').addClass('cha-none');
-      $('div.cha-02').addClass('cha-none');
-      return false; 
-    });
+
+    // csv反映
+    function parseCsv(data) {
+      // CSVを配列で読み込む
+      var csv = $.csv.toArrays(data);
+  
+      var insert = '';
+      $(csv).each(function(i) {
+        if (this.length > 0) {
+          if( i % 2 == 0 ){
+            insert += '<div class="com-txt-right white">';
+            $(this).each(function(i) {
+              if(i === 0){
+                insert += '<h3>' + this + '</h3>';
+              }else{
+                insert += '<p class="hide-01">' + this + '</p>';
+              }
+            });
+            insert += '<div class="wh-01"></div>';
+            insert += '</div>';
+          }else{
+            insert += '<div class="com-txt-left">';
+            $(this).each(function(i) {
+              if(i === 0){
+                insert += '<h3>' + this + '</h3>';
+              }else{
+                insert += '<p class="hide-02">' + this + '</p>';
+              }
+            });
+            insert += '<div class="wh-02"></div>';
+            insert += '</div>';
+          }
+        }
+        $("#result ").append(insert);
+        insert = "";
+      });
+    }
+    // CSVの読み込み
+    $.get('/coment.csv', parseCsv, 'text');
+  
+
 });
